815c7d00c564a46ec5d5e0ff778167cbd45ddd2b
Dvir Hershkovits <dvirhersh@gmail.com>
2025-03-14 14:17:08 +0200

    exercise 16 pipeline - input - 60 bits - no pipelining yet
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

--- logtel/lab16_pipeline/lab16_pipeline.srcs/sources_1/imports/new/pipelining.vhd
+++ logtel/lab16_pipeline/lab16_pipeline.srcs/sources_1/imports/new/pipelining.vhd
@@ -1,4 +1,3 @@
-
 ----------------------------------------------------------------------------------
 -- Company: 
 -- Engineer: 
@@ -19,7 +18,6 @@
 -- 
 ----------------------------------------------------------------------------------
 
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -33,18 +31,17 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity pipelining is
-    Port ( CLOCK : in STD_LOGIC;
-           RESET : in STD_LOGIC;
-           A : in STD_LOGIC_VECTOR (5 downto 0);
-           B : in STD_LOGIC_VECTOR (5 downto 0);
+    Port ( CLOCK    : in  STD_LOGIC;
+           RESET    : in  STD_LOGIC;
+           A        : in  STD_LOGIC_VECTOR (59 downto 0);
+           B        : in  STD_LOGIC_VECTOR (59 downto 0);
            COMP_OUT : out STD_LOGIC);
 end pipelining;
 
 architecture Behavioral of pipelining is
 
-    signal A_sig : STD_LOGIC_VECTOR (5 downto 0) ;
-    signal B_sig : STD_LOGIC_VECTOR (5 downto 0) ;
-    signal out_msb, out_lsb : STD_LOGIC;
+    signal A_sig : STD_LOGIC_VECTOR (59 downto 0) ;
+    signal B_sig : STD_LOGIC_VECTOR (59 downto 0) ;
 
 begin
 
@@ -60,31 +57,11 @@ begin
         end if;
     end process;
 
-    msb : process(CLOCK) begin
-        if rising_edge(CLOCK) then
-            if A_sig(5 downto 3) = B_sig(5 downto 3) then
-                    out_msb <= '1';
-                else
-                    out_msb <= '0';
-            end if;
-        end if;
-    end process;
-
-    lsb : process(CLOCK) begin
-        if rising_edge(CLOCK) then
-            if A_sig(2 downto 0) = B_sig(2 downto 0) then
-                    out_lsb <= '1';
-                else
-                    out_lsb <= '0';
-            end if;
-        end if;
-    end process;
-
     comparison : process(CLOCK) begin
         if rising_edge(CLOCK) then
             if RESET = '1' then   
                 COMP_OUT <= '0';
-            elsif out_msb = '1' and out_lsb = '1' then
+            elsif A_sig = B_sig then
                     COMP_OUT <= '1';
                 else
                     COMP_OUT <= '0';
