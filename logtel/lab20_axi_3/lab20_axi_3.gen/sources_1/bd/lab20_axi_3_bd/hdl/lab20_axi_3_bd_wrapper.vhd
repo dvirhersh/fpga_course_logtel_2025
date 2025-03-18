13fefb7ca2b4118cc6bbafa7ec8606874e653f04
Dvir Hershkovits <dvirhersh@gmail.com>
2025-03-18 14:41:53 +0200

    lab20 step C - can run simulation
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

--- logtel/lab20_axi_3/lab20_axi_3.gen/sources_1/bd/lab20_axi_3_bd/hdl/lab20_axi_3_bd_wrapper.vhd
+++ logtel/lab20_axi_3/lab20_axi_3.gen/sources_1/bd/lab20_axi_3_bd/hdl/lab20_axi_3_bd_wrapper.vhd
@@ -2,7 +2,7 @@
 --Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2024.2 (win64) Build 5239630 Fri Nov 08 22:35:27 MST 2024
---Date        : Tue Mar 18 14:28:16 2025
+--Date        : Tue Mar 18 14:29:52 2025
 --Host        : dvirhersh_comp running 64-bit major release  (build 9200)
 --Command     : generate_target lab20_axi_3_bd_wrapper.bd
 --Design      : lab20_axi_3_bd_wrapper
