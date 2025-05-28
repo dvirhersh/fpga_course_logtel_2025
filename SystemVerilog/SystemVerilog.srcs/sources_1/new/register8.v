c2a6bbde9e5deeb37b21dc850df5c2fb1fb593bc
Dvir Hershkovits <dvirhersh@gmail.com>
2025-05-28 14:48:56 +0300

    for system_verilog
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

---
+++ SystemVerilog/SystemVerilog.srcs/sources_1/new/register8.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/28/2025 02:39:18 PM
+// Design Name: 
+// Module Name: register8
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module register8(
+    input clock,
+    input reset,
+    input enable,
+    input [7:0] data_in,
+    output reg [7:0] data_out
+    );
+    
+    
+   always @(posedge clock)
+      if (reset) begin
+         data_out <= 8'b0;
+      end else if (enable) begin
+         data_out <= data_in;
+      end
+						
+endmodule
